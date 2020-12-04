90e29a464e672fc2fbb0e06a143ea96fd9c92a33
Bry <44911576+Bryl33t@users.noreply.github.com>
2020-12-04 19:14:33 +0100

    Create socket.h

---
+++ includes/socket.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef __linux__
+    #include <sys/socket.h>
+#elif defined __WIN64
+    #include <winsock2.h>
+#endif
+
+typedef enum _bool Bool;
+
+enum _bool {
+   false = 0,
+   true = 1
+};
+
+typedef struct 
+{
+    int *fd;
+} socket_t;
+
+typedef struct 
+{
+    int domain;
+    int type;
+    int proto;
+}config_socket_t;
+
+
+socket_t *socket_init(config_socket_t sock_info);
+void socket_connect(socket_t fd, char *ip, int port, config_socket_t sock_info);
