64dff4b5f936f1a56db6e0a64e72ec185f9a321f
Bry <44911576+Bryl33t@users.noreply.github.com>
2020-12-05 16:03:42 +0100

    using connection socket

--- main.c
+++ main.c
@@ -11,8 +11,16 @@ int main(int argc, char **argv)
     sock_info.type = SOCK_STREAM;
     sock_info.proto = 0; 
 
+    char ip[] = "127.0.0.1\0";
+    int port = 1337;
+
     socket_t *init_sock = socket_init(sock_info);
+
+    printf("fd socket => %d\n", init_sock->fd);
+
+    int conn = socket_connect(init_sock, ip, port, sock_info);
+    printf("conn => %d\n", conn);
     
-    
+    free(init_sock);
     return(0);
 }
